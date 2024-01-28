5c0682eab2ed8578b7b445dc33f0c3832c8ae746
Nurzhan_Makhmut <87759046037b@gmail.com>
2024-01-29 00:38:32 +0600

    add tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] createArray(int size, int min, int max)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+
+    return array;
+}
+
+// Console.Write("ведите размер массива: ");
+int N = new Random().Next(2, 10);
+int [] res = createArray (N, 100, 999);
+
+Console.WriteLine($"Массив: [{string.Join(";", res)}]");
+int sum = 0;
+for (int i = 0; i < res.Length; i++)
+
+{
+    
+    if (res[i]%2 == 0)
+    {
+        sum++;
+    }
+}
+// Console.Write($"Четное количество чисел в массиве: [{string.Join(";", sum)}]");
+ Console.Write($"Четное количество чисел в массиве: { sum }");
---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] createArray(int size, int min, int max)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+
+    return array;
+}
+int N = new Random().Next(2, 10);
+int [] res = createArray (N, 1, 5);
+Console.WriteLine($"Рандомный массив:   [{string.Join(";", res)}]");
+int temp = 0;
+int [] revers = new int [res.Length];
+int a=1;
+for (int i = 0; i < res.Length; i++)
+{
+    revers[i]=res[res.Length-a];
+    a++;
+}
+Console.WriteLine($"Развернутый массив: [{string.Join(";", revers)}]");
